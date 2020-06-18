b9b526f66eccc39a6c4c9020b29a8696f1588443
Outie Dorfling <outie@dorfling.net>
2020-06-18 21:07:01 +0200

    Auto gen files

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.11.0 on 2020-05-20 17:32:01.
+ * Generated for Laravel 7.11.0 on 2020-06-18 19:01:47.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
