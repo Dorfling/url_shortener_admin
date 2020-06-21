d9214c97f39adbe6e6adb2d8920186fa4190e47f
Outie Dorfling <outie@dorfling.net>
2020-06-21 13:57:37 +0200

    Payment Gateway Enum

---
+++ app/Models/Enum/PaymentGatewayEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Enum;
+
+class PaymentGatewayEnum
+{
+    public const PAYFAST_ID = 1;
+    public const PAYFAST_ENUM = '5ba11c62-29bf-4108-aa89-9e2400c4b8a4';
+    public const PAYFAST_NAME = 'Payfast';
+}
