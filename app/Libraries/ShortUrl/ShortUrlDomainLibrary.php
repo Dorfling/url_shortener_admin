272f51471d5f4b4aafc748aeccef39fcf557d01e
Outie Dorfling <outie@dorfling.net>
2020-06-24 22:50:50 +0200

    Adding the domain admin library

---
+++ app/Libraries/ShortUrl/ShortUrlDomainLibrary.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Libraries\ShortUrl;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ShortUrlDomainLibrary
+{
+    /**
+     * @param User $user
+     * @return array
+     * @throws \Exception
+     */
+    public function getAvailableHosts(User $user)
+    {
+        $companies = $user->companies;
+
+        if ($companies->isEmpty()) {
+            Log::error('Companies are empty for user ' . $user->id);
+            throw new \Exception('User does not have companies set');
+        }
+        $sql = 'SELECT uuid, public, domain
+                FROM short_urls.short_url_domains sud
+                WHERE public = true
+                   OR company_id IN (:company_ids)
+                ORDER BY public DESC';
+        $results = DB::select(
+            $sql,
+            [
+                'company_ids' => $companies->implode('id', ','),
+            ]
+        );
+        return $results;
+    }
+}
