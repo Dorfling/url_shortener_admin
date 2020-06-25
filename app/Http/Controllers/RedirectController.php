0260cadfe86f767c6e9dae4082dc49c9b252471e
Outie Dorfling <outie@dorfling.net>
2020-06-26 00:10:45 +0200

    Take the incoming url, find it's full url, add any params that are necessary and redirect away

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Libraries\ShortUrl\ShortUrlLibrary;
+use App\Models\ShortUrl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
 class RedirectController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirect(Request $request)
+    {
+        $url  = \Request::fullUrl();
+        $parts = parse_url($url);
+        $shortUrl = $this->getShortUrlLibrary()->getShortUrlFromUrlParts($parts['host'], $parts['path']);
+        if (!$shortUrl instanceof ShortUrl) {
+            return $this->redirectToHashNotFound();
+        }
+        $fullUrl = $shortUrl->full_url;
+        if (array_key_exists('query', $parts)) {
+            $fullUrl = $this->getShortUrlLibrary()->appendParametersToPath($fullUrl, $parts['query']);
+        }
+
+        return Redirect::away($fullUrl);
+    }
 
     /**
      * @return ShortUrlLibrary
