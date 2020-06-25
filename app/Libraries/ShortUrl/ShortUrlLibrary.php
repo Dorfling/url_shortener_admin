c3f5f8e1f0ab65ed53e24a3e48e3bc3eaca88c89
Outie Dorfling <outie@dorfling.net>
2020-06-25 22:12:31 +0200

    Adding a function to create short urls from a domain and hash

--- app/Libraries/ShortUrl/ShortUrlLibrary.php
+++ app/Libraries/ShortUrl/ShortUrlLibrary.php
@@ -2,7 +2,26 @@
 
 namespace App\Libraries\ShortUrl;
 
+use App\Exceptions\CustomDomainHashExistsException;
+use App\Models\Company;
+use App\Models\ShortUrl;
+use App\Models\ShortUrlDomain;
+use Illuminate\Support\Facades\DB;
+
 class ShortUrlLibrary
 {
+
+    /**
+     * This exists solely to only have one central point to build the short url, lowering chances for mistyping
+     *
+     * @param ShortUrlDomain $shortUrlDomain
+     * @param string $hash
+     * @return string
+     */
+    private function getShortUrlStringFromDomainAndHash(ShortUrlDomain $shortUrlDomain, string $hash):string
+    {
+        return $shortUrlDomain->domain . '/' . $hash;
+    }
+
 
 }
