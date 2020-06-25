f84fbf683c091cf423a9cde87c726f054c3fc52e
Outie Dorfling <outie@dorfling.net>
2020-06-25 22:46:10 +0200

    A function to get a unique hash for a short domain

--- app/Libraries/ShortUrl/ShortUrlLibrary.php
+++ app/Libraries/ShortUrl/ShortUrlLibrary.php
@@ -12,6 +12,33 @@ class ShortUrlLibrary
 {
 
     /**
+     * @param ShortUrlDomain $shortUrlDomain
+     * @param $int
+     * @param int|null $minLength
+     * @return false|string
+     */
+    public function getUniqueHash(ShortUrlDomain $shortUrlDomain, $int, int $minLength = null)
+    {
+        $hash = $this->getShortUrlHashFromId($int);
+        //If a minimum length was set, trim the hash if it's longer than the min length
+        if ($minLength) {
+            if (strlen($hash) <= $minLength) {
+                dump(__LINE__, 'short enough');
+                return $hash;
+            }
+            $hash = substr($hash, 0, $minLength);
+        }
+        //If the shortUrl Exists, do a recursive search to get a new one
+        if ($this->shortURLExists($shortUrlDomain, $hash)) {
+            //Get a random big int
+            $randomNumber = rand(0, PHP_INT_MAX);
+            return $this->getUniqueHash($shortUrlDomain, $randomNumber, $minLength);
+        }
+        //If all went well, pass the hash back
+        return $hash;
+    }
+
+    /**
      * This exists solely to only have one central point to build the short url, lowering chances for mistyping
      *
      * @param ShortUrlDomain $shortUrlDomain
