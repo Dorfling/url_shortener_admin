ee0cd103069f55ff626c5591d738c8e4b675b38c
Outie Dorfling <outie@dorfling.net>
2020-06-26 00:10:07 +0200

    Adding the short url library to the controller

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\ShortUrl\ShortUrlLibrary;
 class RedirectController extends Controller
 {
 
+    /**
+     * @return ShortUrlLibrary
+     */
+    private function getShortUrlLibrary(): ShortUrlLibrary
+    {
+        return new ShortUrlLibrary();
+    }
 }
